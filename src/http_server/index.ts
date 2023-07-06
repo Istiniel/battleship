03b854e44f34e91cfaf83e9f001f19e4b6b81c99
Istiniel <eclipsio@mail.ru>
2023-07-06 18:45:15 +0300

    fix: fix relative path to frontend files

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -4,14 +4,18 @@ import * as http from 'http'
 
 export const httpServer = http.createServer(function (req, res) {
   const dirname = path.resolve(path.dirname(''))
-  const filePath = dirname + (req.url === '/' ? '/front/index.html' : `/front${req.url ?? ''}`)
+  const filePath = path.resolve(
+    dirname,
+    req.url === '/' ? 'front/index.html' : `front${req.url ?? ''}`,
+  )
+
   fs.readFile(filePath, function (err, data) {
-    if (err !== undefined) {
+    if (err !== undefined && err !== null) {
       res.writeHead(404)
       res.end(JSON.stringify(err))
       return
     }
-    res.writeHead(200)
+    res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end(data)
   })
 })
