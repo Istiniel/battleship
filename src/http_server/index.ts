1fc5768df66b56a069184821834b2232c661b680
Istiniel <eclipsio@mail.ru>
2023-07-06 18:19:50 +0300

    init: prepare boilerplate for task

---
+++ src/http_server/index.ts
@@ -0,0 +1,17 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import * as http from 'http'
+
+export const httpServer = http.createServer(function (req, res) {
+  const dirname = path.resolve(path.dirname(''))
+  const filePath = dirname + (req.url === '/' ? '/front/index.html' : `/front${req.url ?? ''}`)
+  fs.readFile(filePath, function (err, data) {
+    if (err !== undefined) {
+      res.writeHead(404)
+      res.end(JSON.stringify(err))
+      return
+    }
+    res.writeHead(200)
+    res.end(data)
+  })
+})
